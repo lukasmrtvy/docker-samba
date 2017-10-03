8813a41dc0c1595780ae4b97b3b6ba202385d8ff
Lukas M <lukas.mrtvy@gmail.com>
2017-10-03 21:05:45 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-pdbedit  -L -u ${USER}
-
-if [[  $? -ne 0 ]]; then
-  echo -e "${PWD}\n${PWD}" | smbpasswd -a -s -c /config/smb.conf ${USER}
+if [[ -n ${PWD} ]];then
+  pdbedit  -L -u ${USER}
+  if [[  $? -ne 0 ]]; then
+    echo -e "${PWD}\n${PWD}" | smbpasswd -a -s -c /config/smb.conf ${USER}
+  fi
+else
+  echo "env variable PWD is not supplied"
+  exit 1
 fi
 
 exec /usr/bin/supervisord -c /config/supervisord.conf
